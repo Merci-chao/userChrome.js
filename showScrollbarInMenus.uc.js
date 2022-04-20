768acd4e9608bb0a7fe37191520e48dc1e456c0f
Merci-chao <Merci-chao@users.noreply.github.com>
2022-04-20 12:00:43 +0800

    Create showScrollbarInMenus.uc.js

--- showScrollbarInMenus.uc.js
+++ showScrollbarInMenus.uc.js
@@ -5,18 +5,10 @@ addEventListener("popupshowing", ({target}) => {
 	if (box && !box.dataset["noarrow"]) {
 		box.dataset["noarrow"] = true;
 		box._scrollButtonDown.style.visibility = box._scrollButtonUp.style.visibility = "collapse";
-		if (target.matches(":is(.subviewbutton, .subviewbutton-nav) menupopup")) {
-			let innerbox = box.shadowRoot.querySelector("scrollbox");
-			innerbox.style.setProperty("margin-top", 0, "important");
-			innerbox.style.setProperty("margin-bottom", 0, "important");
-			innerbox.style.setProperty("overflow-y", "auto", "important");
-		}
+		let innerbox = box.shadowRoot.querySelector("scrollbox");
+		innerbox.style.setProperty("margin-top", 0, "important");
+		innerbox.style.setProperty("margin-bottom", 0, "important");
+		innerbox.style.setProperty("overflow-y", "auto", "important");
 	}
 }, true);
-document.body.appendChild(document.createElement("style")).innerHTML = `
-menupopup::part(arrowscrollbox-scrollbox),
-menupopup menupopup::part(arrowscrollbox-scrollbox) {
-	overflow-y: auto;
-}
-`;
 })()} catch(e) {alert(e);console.error(e)}
