77f3742239310e54b039fc7515416b80a470d998
Merci chao <Merci-chao@users.noreply.github.com>
2025-08-15 21:02:59 +0800

    Update SemiFullScreen@Merci.chao.uc.js

--- SemiFullScreen@Merci.chao.uc.js
+++ SemiFullScreen@Merci.chao.uc.js
@@ -1,19 +1,23 @@
-/*
-update 2024/11/13: display settings in about:config
-update 2024/05/16: recover the border top of window in pip mode
-update 2023/05/14: udpate for fx 113
-update 2022/08/22: update for fx 103
-update 2022/06/08: update for fx 101
-update 2022/04/29: now minimize then restore window won't exit the fullscreen state
-*/
 "use strict";
+// ==UserScript==
+// @name          Semi-Full Screen
+// @version        2025-08-15
+// @author         Merci chao
+// @namespace      https://github.com/Merci-chao/userChrome.js#semi-full-screen--picture-in-picture-mode
+// @supportURL     https://github.com/Merci-chao/userChrome.js/issues/new
+// @updateURL      https://github.com/Merci-chao/userChrome.js/raw/refs/heads/main/SemiFullScreen@Merci.chao.uc.js
+// @downloadURL    https://github.com/Merci-chao/userChrome.js/raw/refs/heads/main/SemiFullScreen@Merci.chao.uc.js
+// ==/UserScript==
+
 if (location == "chrome://browser/content/browser.xhtml") try {(()=>{
 
 let prefs;
+let prefBranchStr = "extensions.SemiFullScreen@Merci.chao.";
 {
-	let prefBranchStr = "extensions.SemiFullScreen@Merci.chao.";
 	let defPrefs = {
 		reverse: false,
+		checkUpdate: 1,
+		checkUpdateFrequency: 7,
 	};
 
 	let setDefaultPrefs = (branch, data) => Object.entries(data).forEach(([name, value]) =>
@@ -25,29 +29,72 @@ let prefs;
 	prefs = getPrefs(Services.prefs.getBranch(prefBranchStr), defPrefs);
 }
 
+
+if (prefs.checkUpdate && (Date.now() / 1000 - prefs.checkUpdate) / 60 / 60 / 24 >= Math.max(prefs.checkUpdateFrequency, 1)) {
+	Services.prefs.setIntPref(prefBranchStr + "checkUpdate", Date.now() / 1000);
+	(async () => {
+		let getVer = code => (code.match(/^\/\/\s*@version\s+(.+)$/mi) || [])[1];
+		let localScript = await (await fetch(new Error().stack.match(/(?<=@).+?(?=:\d+:\d+$)/m)[0])).text();
+		let updateURL = localScript.match(/^\/\/\s*@updateURL\s+(.+)$/mi)[1];
+		let downloadURL = localScript.match(/^\/\/\s*@downloadURL\s+(.+)$/mi)[1];
+		let remoteScript = await (await fetch(updateURL)).text();
+		let local = getVer(localScript);
+		let remote = getVer(remoteScript);
+		if (remote.localeCompare(local, undefined, {numeric: true}) <= 0) return;
+		let p = Services.prompt;
+		let buttons = p.BUTTON_POS_0 * p.BUTTON_TITLE_YES +
+				p.BUTTON_POS_1 * p.BUTTON_TITLE_IS_STRING +
+				p.BUTTON_POS_2 * p.BUTTON_TITLE_NO;
+		let dontAsk = {};
+		let l10n = {
+			en: {
+				title: "Update Notification",
+				message: `Semi-Full Screen version ${remote} is released. Would you want to view it now?`,
+				later: "Remind Tomorrow",
+				never: `Stop checking when selecting "No" (strongly not recommended)`,
+			},
+		};
+		l10n = l10n[Services.locale.appLocaleAsLangTag.split("-")[0]] || l10n.en;
+		switch (p.confirmEx(window, l10n.title, l10n.message,
+				buttons, "", l10n.later, "", l10n.never, dontAsk)) {
+			case 0:
+				openURL(downloadURL);
+				break;
+			case 1:
+				Services.prefs.setIntPref(prefBranchStr + "checkUpdate",
+						Date.now() / 1000 - (Math.max(prefs.checkUpdateFrequency, 1) - 1) * 24 * 60 * 60);
+				break;
+			case 2:
+				if (dontAsk.value)
+					Services.prefs.setIntPref(prefBranchStr + "checkUpdate", 0);
+				break;
+		}
+	})();
+}
+
 function SemiFullScreen(window) {
 	Object.assign(this, {
 		window,
-		
+
 		on: false,
 		normalSizeBefore: false,
 		previousSizeMode: null,
 		shift: false,
 		ctrl: false,
-		
+
 		originalFullScreen: null,
 		styleElt: null,
 		hideToolboxTimeout: null,
 	});
-	
+
 	this.bind();
 }
 
 SemiFullScreen.prototype = {
 	HANDLED_EVENTS: ["keydown", "keyup", "deactivate"],
-	
+
 	LAZY_HANDLED_EVENTS: ["mouseleave", "mouseenter", "sizemodechange"],
-	
+
 	bind() {
 		let {window} = this;
 		window[SemiFullScreen.SYMBOL] = this;
@@ -58,17 +105,17 @@ SemiFullScreen.prototype = {
 			}
 		});
 		this.HANDLED_EVENTS.forEach(e => window.addEventListener(e, this, false));
-		
+
 		this.originalFullScreen = Object.getOwnPropertyDescriptor(window, "fullScreen");
-		
+
 		Object.defineProperty(window, "fullScreen", {
 			get: () => this.on || this.originalFullScreen.get.call(this.window),
-			
+
 			set: val => {
 				let {window} = this, {FullScreen, document} = window,
 						root = document.documentElement;
 				let handled = true;
-				
+
 				if (val != window.fullScreen)
 					if (this.on) {
 						this.LAZY_HANDLED_EVENTS.forEach(e => window.removeEventListener(e, this, true));
@@ -87,37 +134,37 @@ SemiFullScreen.prototype = {
 							pip = false;
 						if (!pip)
 							window.maximize();
-						
+
 						if (/firefox-compact-(light|dark)@mozilla\.org/.test(Services.prefs.getCharPref("extensions.activeThemeID")))
 							root.setAttribute("semi-fullscreen-transparent", true);
-						
+
 						let borderWidth =  pip && navigator.oscpu.startsWith("Windows NT 1") ?
 								0 : (window.outerWidth - root.clientWidth) / 2;
 						let style = `
 							:root {
 								--semi-fullscreen-border-width: ${borderWidth}px;
 							}
-							
+
 							@media (-moz-platform: windows-win7), (-moz-platform: windows-win8) {
 								:root[semi-fullscreen-transparent][semi-fullscreen-win='7'] #titlebar,
 								:root[sizemode=maximized][semi-fullscreen-win='7'],
 								:root[sizemode=maximized][semi-fullscreen-win='8'] #titlebar {
 									margin-top: calc(0px - var(--semi-fullscreen-border-width)) !important;
 								}
-								
+
 								:root:is([sizemode=maximized], [semi-fullscreen-transparent][semi-fullscreen-win='7']) {
 									margin-top: var(--semi-fullscreen-border-width) !important;
 								}
-								
+
 								:root:is([sizemode=maximized], [semi-fullscreen-transparent][semi-fullscreen-win='7']) #fullscr-toggler {
 									top: var(--semi-fullscreen-border-width) !important;
 								}
-								
+
 								:root:is([sizemode=maximized], [semi-fullscreen-transparent]) {
 									height: calc(100vh - var(--semi-fullscreen-border-width)) !important;
 								}
 							}
-							
+
 							@media not (-moz-platform: windows-win7) {
 								@media not (-moz-platform: windows-win8) {
 									.titlebar-buttonbox {
@@ -125,7 +172,7 @@ SemiFullScreen.prototype = {
 									}
 								}
 							}
-							
+
 							@media (-moz-windows-default-theme) {
 								@media (-moz-windows-compositor) {
 									@media (-moz-platform: windows-win8) {
@@ -134,7 +181,7 @@ SemiFullScreen.prototype = {
 											height: 20px;
 										}
 									}
-								
+
 									@media (-moz-platform: windows-win7) {
 										.titlebar-buttonbox {
 											width: 0;
@@ -143,7 +190,7 @@ SemiFullScreen.prototype = {
 									}
 								}
 							}
-							
+
 							.titlebar-button {
 								display: flex;
 							}
@@ -151,42 +198,38 @@ SemiFullScreen.prototype = {
 						if (pip)
 							style += `
 								#fullscr-toggler {
-									background: AccentColor;
-									display: block !important;
+									display: flex !important;
 								}
-								#fullscr-toggler:-moz-window-inactive {
-									background: rgb(57,57,57);
-								}
-								
-								:root[inFullscreen] .titlebar-spacer {
-									display: block !important;
+
+								:root[inFullscreen] #TabsToolbar .titlebar-spacer {
+									display: flex !important;
 								}
 							`;
 						this.styleElt = document.body.appendChild(document.createElement("style"));
 						this.styleElt.innerHTML = style;
-						
+
 						this.LAZY_HANDLED_EVENTS.forEach(e => window.addEventListener(e, this, true));
 					} else {
 						this.originalFullScreen.set.call(window, val);
 						handled = false;
 					}
-				
+
 				if (handled)
 					this.window.dispatchEvent(new window.Event("fullscreen", {bubbles: true, cancelable: true}));
 			},
-			
+
 			configurable: true,
 			enumerable: true,
 		});
 	},
-	
+
 	clearHideToolboxTimeout() {
 		if (this.hideToolboxTimeout) {
 			window.clearTimeout(this.hideToolboxTimeout);
 			this.hideToolboxTimeout = null;
 		}
 	},
-	
+
 	handleEvent(e) {
 		let {type, key, shiftKey, ctrlKey, clientY, target} = e, {window} = this;
 		switch (type) {
