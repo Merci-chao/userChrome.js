7bf097ba2925b617269aa7415d17924c84f35c15
Merci chao <Merci-chao@users.noreply.github.com>
2025-12-09 22:15:34 +0800

    Update the update-checking feature

--- FloatToolbarsInFullScreen@Merci.chao.uc.js
+++ FloatToolbarsInFullScreen@Merci.chao.uc.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name           Float Toolbars in Full Screen
+// @description    Float the toolbars over the page in full screen mode, instead of making the web page jumpy when the toolbars showing / hiding.
 // @version        2025-08-16
 // @author         Merci chao
-// @namespace      https://github.com/Merci-chao/userChrome.js#float-toolbars-in-full-screen
+// @homepageURL    https://github.com/Merci-chao/userChrome.js#float-toolbars-in-full-screen
+// @changelogURL   https://github.com/Merci-chao/userChrome.js#changelog-4
 // @supportURL     https://github.com/Merci-chao/userChrome.js/issues/new
 // @updateURL      https://github.com/Merci-chao/userChrome.js/raw/refs/heads/main/FloatToolbarsInFullScreen@Merci.chao.uc.js
-// @downloadURL    https://github.com/Merci-chao/userChrome.js/raw/refs/heads/main/FloatToolbarsInFullScreen@Merci.chao.uc.js
 // ==/UserScript==
 
 try {
+const SCRIPT_NAME = "Float Toolbars in Full Screen";
+const SCRIPT_FILE_NAME = "FloatToolbarsInFullScreen@Merci.chao.uc.js";
 
 let prefBranchStr = "extensions.FloatToolbarsInFullScreen@Merci.chao.";
 let defPrefs = {
@@ -23,45 +26,136 @@ let getPrefs = (branch, data) => Object.fromEntries(Object.entries(data).map(([n
 setDefaultPrefs(Services.prefs.getDefaultBranch(prefBranchStr), defPrefs);
 let prefs = getPrefs(Services.prefs.getBranch(prefBranchStr), defPrefs);
 
-
 if (prefs.checkUpdate && (Date.now() / 1000 - prefs.checkUpdate) / 60 / 60 / 24 >= Math.max(prefs.checkUpdateFrequency, 1)) {
 	Services.prefs.setIntPref(prefBranchStr + "checkUpdate", Date.now() / 1000);
 	(async () => {
-		let getVer = code => (code.match(/^\/\/\s*@version\s+(.+)$/mi) || [])[1];
-		let localScript = await (await fetch(new Error().stack.match(/(?<=@).+?(?=:\d+:\d+$)/m)[0])).text();
-		let updateURL = localScript.match(/^\/\/\s*@updateURL\s+(.+)$/mi)[1];
-		let downloadURL = localScript.match(/^\/\/\s*@downloadURL\s+(.+)$/mi)[1];
-		let remoteScript = await (await fetch(updateURL)).text();
+		let getVer = code => code?.match(/^\/\/\s*@version\s+(.+?)\s*$/mi)?.[1];
+		let localFileURI = new Error().stack.match(/(?<=@).+?(?=:\d+:\d+$)/m)[0];
+		let localFilePath = decodeURI(localFileURI.replace(/^file:\/\/\/|\?.*$/g, "")).replaceAll("/", "\\");
+		let localScript = await (await fetch(localFileURI)).text();
+		let updateURL = localScript.match(/^\/\/\s*@updateURL\s+(.+?)\s*$/mi)[1];
+		let homeURL = "https://github.com/Merci-chao/userChrome.js";
+		let remoteScript = (await (await fetch(updateURL)).text()).trim();
 		let local = getVer(localScript);
 		let remote = getVer(remoteScript);
-		if (remote.localeCompare(local, undefined, {numeric: true}) <= 0) return;
-		let p = Services.prompt;
-		let buttons = p.BUTTON_POS_0 * p.BUTTON_TITLE_YES +
-				p.BUTTON_POS_1 * p.BUTTON_TITLE_IS_STRING +
-				p.BUTTON_POS_2 * p.BUTTON_TITLE_NO;
-		let dontAsk = {};
+		if (
+			!remote ||
+			remote.localeCompare(local, undefined, {numeric: true}) <= 0
+		)
+			return;
+
 		let l10n = {
 			en: {
-				title: "Update Notification",
-				message: `Float Toolbars in Full Screen version ${remote} is released. Would you want to view it now?`,
+				message: `${SCRIPT_NAME} (${SCRIPT_FILE_NAME}) version ${remote} is released.`,
+				update: "Update Now",
+				updateKey: "N",
+				download: "Update Manually",
+				downloadKey: "M",
+				changelog: "Changelog",
+				changelogKey: "C",
 				later: "Remind Tomorrow",
-				never: `Stop checking when selecting "No" (strongly not recommended)`,
+				laterKey: "R",
+				link: "#changelog-4",
+				done: `${SCRIPT_NAME} has been updated to version ${remote}. You may restart Firefox to apply.`,
+				error: `Failed to apply the update of ${SCRIPT_NAME} version ${remote}. Please ensure the file is not read-only or locked by another program:`,
+			},
+			ja: {
+				message: `${SCRIPT_NAME}（${SCRIPT_FILE_NAME}）の新バージョン ${remote} がリリースされました。`,
+				update: "今すぐ更新",
+				updateKey: "N",
+				download: "手動で更新",
+				downloadKey: "M",
+				changelog: "変更履歴",
+				changelogKey: "C",
+				later: "明日再通知する",
+				laterKey: "R",
+				link: "/blob/main/README.jp.md#変更履歴-4",
+				done: `${SCRIPT_NAME} ${remote} を更新しました。Firefox を再起動すると変更が有効になります。`,
+				error: `${SCRIPT_NAME} バージョン ${remote} の更新処理に失敗しました。ファイルが読み取り専用でないこと、または他のプログラムによってロックされていないことを確認してください：`,
 			},
 		};
 		l10n = l10n[Services.locale.appLocaleAsLangTag.split("-")[0]] || l10n.en;
-		switch (p.confirmEx(window, l10n.title, l10n.message,
-				buttons, "", l10n.later, "", l10n.never, dontAsk)) {
-			case 0:
-				openURL(downloadURL);
-				break;
-			case 1:
-				Services.prefs.setIntPref(prefBranchStr + "checkUpdate",
-						Date.now() / 1000 - (Math.max(prefs.checkUpdateFrequency, 1) - 1) * 24 * 60 * 60);
-				break;
-			case 2:
-				if (dontAsk.value)
-					Services.prefs.setIntPref(prefBranchStr + "checkUpdate", 0);
-				break;
+
+		showNotification(
+			l10n.message,
+			[
+				{
+					label: l10n.update,
+					accessKey: l10n.updateKey,
+					callback: install,
+					primary: true,
+				},
+				{
+					label: l10n.download,
+					accessKey: l10n.downloadKey,
+					callback: showChangelog,
+				},
+				{
+					label: l10n.later,
+					accessKey: l10n.laterKey,
+					callback: () => Services.prefs.setIntPref(
+						prefBranchStr + "checkUpdate",
+						Date.now() / 1000 - (Math.max(prefs.checkUpdateFrequency, 1) - 1) * 24 * 60 * 60,
+					),
+				},
+			],
+			"chrome://browser/skin/update-badge.svg",
+		);
+
+		async function showNotification(label, buttons, icon) {
+			let box = await gNotificationBox.appendNotification(
+				"multitabrows",
+				{
+					label,
+					priority: gNotificationBox.PRIORITY_INFO_HIGH,
+				},
+				buttons,
+				true,
+			);
+			if (icon) {
+				let node = box.shadowRoot.querySelector(".icon");
+				let color = "var(--panel-banner-item-update-supported-bgcolor)";
+				node.src = icon;
+				Object.assign(node.style, {
+					fill: color,
+					color,
+				});
+				node.style.setProperty("--message-bar-icon-url", `url(${icon})`);
+			}
+		}
+
+		function install() {
+			try {
+				/*global FileUtils*/
+				let fos = FileUtils.openFileOutputStream(
+					FileUtils.File(localFilePath),
+					FileUtils.MODE_WRONLY | FileUtils.MODE_TRUNCATE,
+				);
+				let converter = Cc["@mozilla.org/intl/converter-output-stream;1"]
+					.createInstance(Ci.nsIConverterOutputStream);
+				converter.init(fos, "UTF-8");
+				converter.writeString(remoteScript);
+				converter.close();
+
+				//Delay a bit to make the installation feel like it's actually running
+				setTimeout(() => showNotification(
+					l10n.done,
+					[
+						{
+							label: l10n.changelog,
+							accessKey: l10n.changelogKey,
+							callback: showChangelog,
+						},
+					],
+				), 500);
+			} catch(e) {
+				Services.prompt.alert(window, l10n.title, [l10n.error, localFilePath, e.message].join("\n\n"));
+				return true;
+			}
+		}
+
+		function showChangelog() {
+			openURL(homeURL + l10n.link);
 		}
 	})();
 }
@@ -269,4 +363,3 @@ function needToHideBackground() {
 }
 
 } catch(e) {alert(["FloatToolbarsInFullScreen@Merci.chao.uc.js",e,e.stack].join("\n"));console.error(e)}
-
