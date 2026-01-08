fd5a4bd275386afdbafb6dde66e6a1911187fe5d
Merci chao <Merci-chao@users.noreply.github.com>
2026-01-08 10:51:40 +0800

    Update showScrollbarInMenus.uc.js

--- showScrollbarInMenus.uc.js
+++ showScrollbarInMenus.uc.js
@@ -3,8 +3,14 @@ addEventListener("popupshowing", ({target}) => {
 	let {scrollBox} = target;
 	if (!scrollBox.dataset["noarrow"]) {
 		scrollBox.dataset["noarrow"] = true;
-		scrollBox._scrollButtonDown.style.visibility = scrollBox._scrollButtonUp.style.visibility = "collapse";
-		for (let [p, v] of [["margin-block", 0], ["padding-block", 0], ["overflow-y", "auto"]])
+		scrollBox._scrollButtonDown.style.visibility = 
+			scrollBox._scrollButtonUp.style.visibility = "collapse";
+		for (let [p, v] of [
+			["margin-block", 0],
+			["padding-block", 0],
+			["border-block", 0],
+			["overflow-y", "auto"],
+		])
 			scrollBox.scrollbox.style.setProperty(p, v, "important");
 	}
 }, true);
