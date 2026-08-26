da5a37d8113943a8aaa2d14de2e76849acbe40d3
Merci chao <Merci-chao@users.noreply.github.com>
2026-08-26 16:07:38 +0800

    Add support for bookmarks toolbar

--- BookmarksMenuItemIconClickable@Merci.chao.uc.js
+++ BookmarksMenuItemIconClickable@Merci.chao.uc.js
@@ -1,15 +1,16 @@
 if (document.documentElement.id == "main-window") {
-	let popups = ":is(#bookmarksMenuPopup, #BMB_bookmarksPopup)";
-	let menuitems = `${popups} .bookmark-item:not([id])`;
-	let selector = `${menuitems} :is(.menu-icon, .menu-iconic-left)`;
+	let containers = ":is(#bookmarksMenuPopup, #BMB_bookmarksPopup, #PlacesToolbarItems)";
+	let items = `${containers} .bookmark-item:not([id])`;
+	let icons = `${items} :is(.menu-icon, .menu-iconic-left, .toolbarbutton-icon)`;
 	document.body.appendChild(document.createElement("style")).textContent = /*css*/`
-		${menuitems} {
+		${items} {
 			overflow: clip;
 		}
-	
-		${selector} {
+
+		${icons} {
 			--block: var(--panel-menuitem-padding-block, var(--arrowpanel-menuitem-padding-block));
 			--inline: var(--panel-menuitem-padding-inline, var(--arrowpanel-menuitem-padding-inline));
+			--margin-end: var(--space-small, 8px);
 			pointer-events: auto;
 			border-radius: var(--menuitem-border-radius, var(--arrowpanel-menuitem-border-radius));
 			box-sizing: content-box;
@@ -17,24 +18,32 @@ if (document.documentElement.id == "main-window") {
 			margin-block: calc(var(--block) * -1 - env(hairline, 1px)) !important;
 			margin-inline:
 				calc(var(--inline) * -1 - env(hairline, 1px))
-				calc(var(--space-small, 8px) - var(--inline) - env(hairline, 1px)) !important;
+				calc(var(--margin-end) - var(--inline) - env(hairline, 1px)) !important;
 			padding-block: var(--block) !important;
 			padding-inline: var(--inline) !important;
 			overflow: visible;
 			background-clip: padding-box;
 		}
 
-		#bookmarksMenuPopup${selector} {
+		#bookmarksMenuPopup${icons} {
 			--block: .5em;
 			--inline: .5em;
 			border-radius: var(--menuitem-border-radius);
 		}
 
-		${selector}:hover {
+		${icons}.toolbarbutton-icon {
+			--block: var(--bookmark-block-padding);
+			--inline: 4px;
+			--margin-end: 4px;
+			border-radius: var(--toolbarbutton-border-radius);
+			height: 100%;
+		}
+
+		${icons}:hover {
 			background-color: var(--toolbarbutton-background-color-hover, var(--toolbarbutton-hover-background));
 		}
 
-		${selector}:hover:active {
+		${icons}:hover:active {
 			background-color: var(--toolbarbutton-background-color-active, var(--toolbarbutton-active-background));
 		}
 	`;
@@ -46,14 +55,14 @@ if (document.documentElement.id == "main-window") {
 		addEventListener("DOMContentLoaded", init, {once: true});
 
 	function init() {
-		for (let p of [...document.querySelectorAll(popups)])
+		for (let p of [...document.querySelectorAll(containers)])
 			p.addEventListener("click", aEvent => {
-				if (aEvent.button > 1 || !aEvent.target.closest(selector))
+				if (aEvent.button > 1 || !aEvent.target.closest(icons))
 					return;
 
 				let item = aEvent.target.closest(".bookmark-item");
 				item.dispatchEvent(new MouseEvent(
-					item.tagName == "menu" ? "click" : "command",
+					item.matches("menu, [type=menu]") ? "click" : "command",
 					{
 						bubbles: true,
 						cancelable: true,
